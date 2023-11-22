699542a52505a8c7c32326ee1cc2de94f81e3fcf
art_bir <art.bir.1987@gmail.com>
2023-11-22 18:21:15 +0300

    add tests

--- src/main.py
+++ src/main.py
@@ -4,17 +4,19 @@ from utils import *
 def main():
     """Функция вывода последних пяти EXECUTED транзакций"""
 
+    # Количество транзакций
     transactions_number = 5
 
     tran_list = read_json()
     for i in range(transactions_number):
         tran_dict = tran_list[i]
 
+        # Проверяет EXECUTED
         if tran_dict['state'] == 'EXECUTED':
-
             to_account = trans_card_account(tran_dict['to'], 'to')
             date = trans_date(tran_dict['date'])
 
+            # Проверяет наличие from в словаре
             if list(tran_dict)[5] == 'from':
                 from_account = trans_card_account(tran_dict['from'], 'from')
                 print(f"""
@@ -22,7 +24,6 @@ def main():
                      {from_account} -> {to_account}
                      {tran_dict['operationAmount']['amount']} {tran_dict['operationAmount']['currency']['name']}
                  """)
-
             else:
                 print(f"""
                       {date} {tran_dict['description']}
